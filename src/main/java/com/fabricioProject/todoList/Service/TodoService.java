7e24b851d78bb5602c60b584800b42b0859cfb3d
Fabricio C Santos <fsantos.veredinha@gmail.com>
2025-07-21 20:07:28 -0300

    Criação do service

---
+++ src/main/java/com/fabricioProject/todoList/Service/TodoService.java
@@ -0,0 +1,41 @@
+package com.fabricioProject.todoList.Service;
+
+import com.fabricioProject.todoList.Entity.TodoModel;
+import com.fabricioProject.todoList.Repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TodoService {
+
+    private TodoRepository todoRepository;
+
+    public TodoService(TodoRepository todoRepository){
+        this.todoRepository = todoRepository;
+    }
+
+    public List<TodoModel> create(TodoModel todoModel){
+        todoRepository.save(todoModel);
+        return list();
+    }
+
+    public List<TodoModel> list(){
+        Sort sort = Sort.by("prioridade").descending().and(
+                Sort.by("nome").ascending());
+        return todoRepository.findAll(sort);
+    }
+
+    public List<TodoModel> update(TodoModel todoModel){
+        todoRepository.save(todoModel);
+        return list();
+    }
+
+    public List<TodoModel> delete(Long id){
+        todoRepository.deleteById(id);
+        return list();
+    }
+
+}
